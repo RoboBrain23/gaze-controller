116fb7bce4bf5c312214d756084d1a503c4c925c
Ahmed Hamada <ahmedhamada507@gmail.com>
2022-12-07 01:43:31 +0200

    add gaze

--- eye.py
+++ eye.py
@@ -33,13 +33,12 @@ class Eye:
 
         return (int((p1[0]+p2[0])/2),int((p1[1]+p2[1])/2))
 
-    def get_eye_region(self,points):
+    def get_eye_region(self):
         """Returns Eye region from points
         Arguments:
             points: Eye points
-
         """
-
+        points = self.__eye
         eye_region = np.array([ (self.__landmarks.part(points[0]).x,self.__landmarks.part(points[0]).y),
                             (self.__landmarks.part(points[1]).x,self.__landmarks.part(points[1]).y),
                             (self.__landmarks.part(points[2]).x,self.__landmarks.part(points[2]).y),
@@ -71,7 +70,7 @@ class Eye:
         Arguments:
             points : Eye points
         """
-        points = self.get_eye_region(self.__eye)
+        points = self.get_eye_region()
         left_point = points[0]
         right_point = points[3]
         horizontal_distance = math.hypot(left_point[0]-right_point[0],
@@ -84,7 +83,7 @@ class Eye:
         Arguments:
             points : Eye points
         """
-        points = self.get_eye_region(self.__eye)
+        points = self.get_eye_region()
         mid_point_up = self.mid_point(points[1],points[2])
         mid_point_down = self.mid_point(points[4],points[5])
         vertical_distance = math.hypot(mid_point_up[0]-mid_point_down[0],
@@ -110,6 +109,61 @@ class Blink:
     def get_closed_eyes_frame(self):
         return self.__closed_eyes_frame
 
+class Gaze:
+    def __init__(self,eye_region,mask,gray):
+        self.eye_region = eye_region
+        self.__threshold = 45
+        self.mask = mask
+        self.gray = gray
+
+    def get_gaze_ratio(self):
+        eye_threshold = self.get_eye_threshold(self.eye_region)
+        height,width = eye_threshold.shape
+        left_side_eye_threshold = eye_threshold[0:height,0:int(width/3)]
+        left_side_eye_threshold_white = cv2.countNonZero(left_side_eye_threshold)
+
+        center_side_eye_threshold = eye_threshold[0:height,int(width/3):int(2*width/3)]
+        center_side_eye_threshold_white = cv2.countNonZero(center_side_eye_threshold)
+
+        right_side_eye_threshold = eye_threshold[0:height,int(2*width/3):]
+        right_side_eye_threshold_white = cv2.countNonZero(right_side_eye_threshold)
+
+        #left
+        if left_side_eye_threshold_white < right_side_eye_threshold_white and left_side_eye_threshold_white< center_side_eye_threshold_white:
+            gaze_ratio = 0
+        #right
+        elif left_side_eye_threshold_white > right_side_eye_threshold_white and right_side_eye_threshold_white < center_side_eye_threshold_white:
+            gaze_ratio = 2
+        #center
+        else:
+            gaze_ratio = 1
+        return gaze_ratio
+
+    def get_min_max_eye_region(self):
+        min_x = np.min(self.eye_region[:,0])
+        max_x = np.max(self.eye_region[:,0])
+        min_y = np.min(self.eye_region[:,1])
+        max_y = np.max(self.eye_region[:,1])
+        return min_x,max_x,min_y,max_y
+
+
+    def get_eye_threshold(self,eye_region):
+        # eye_region = get_eye_region(points,landmarks)
+        min_x,max_x,min_y,max_y = self.get_min_max_eye_region()
+        cv2.polylines(self.mask,[eye_region],True,0,5)
+        cv2.fillPoly(self.mask,[eye_region],255)
+        gray_eye = cv2.bitwise_and(self.gray,self.gray,mask=self.mask)
+        eye = gray_eye[min_y:max_y,min_x:max_x]
+        eye = cv2.resize(eye, None,fx=5,fy=5)
+        _ , eye_threshold = cv2.threshold(eye,self.__threshold,255,cv2.THRESH_BINARY)
+        cv2.imshow('eye_threshold',eye_threshold)
+        return eye_threshold
+
+    def set_threshold(self,threshold):
+        self.__threshold = threshold
+    
+    def get_threshold(self):
+        return self.__threshold
 
 if __name__ == "__main__":
     TOTAL_BLINKS =0
@@ -139,7 +193,7 @@ if __name__ == "__main__":
             right_eye = Eye(frame,'right',landmarks)
             # Detect blinking
             left_eye_ratio = left_eye.blink_ratio()
-            print(left_eye_ratio)
+            # print(left_eye_ratio)
             right_eye_ratio = right_eye.blink_ratio()
             blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
             blink = Blink(blinking_ratio)
@@ -153,29 +207,16 @@ if __name__ == "__main__":
                     BLINKS_COUNTER =0
             cv2.putText(frame,  f'Total Blinks: {TOTAL_BLINKS}',(50, 350), font, 2, (0, 0, 255), 3)
 
-
-            # left_ratio = get_blink_ratio(LEFT_EYE_POINTS,landmarks)
-            # right_ratio = get_blink_ratio(RIGHT_EYE_POINTS,landmarks)
-            # blink_ratio = (left_ratio+right_ratio)/2
-            # # print(blink_ratio)
-            # if(blink.is_blinking()):
-            #     cv2.putText(frame,"Blinking",(50,150),font,3,(255,0,0))
-            # #Need Calibration
-            # #gaze_ratio = (get_gaze_ratio(RIGHT_EYE_POINTS,landmarks)+get_gaze_ratio(LEFT_EYE_POINTS,landmarks))/2
-            # right_rat=get_gaze_ratio_v2(RIGHT_EYE_POINTS,landmarks,frame,gray,mask)
-            # left_rat = get_gaze_ratio_v2(LEFT_EYE_POINTS,landmarks,frame,gray,mask)
-            # print("left : ",left_rat," right : ",right_rat)
-        
-            # gaze_ratio = ceil((get_gaze_ratio_v2(RIGHT_EYE_POINTS,landmarks,frame,gray,mask)+get_gaze_ratio_v2(LEFT_EYE_POINTS,landmarks,frame,gray,mask))//2)
-            # print(gaze_ratio)
-        
-            # if gaze_ratio == 2:
-            #     cv2.putText(frame, "STATE : RIGHT", (350, 50), font, 1, (0, 0, 255), 3)
-            # elif gaze_ratio == 1:
-            #     cv2.putText(frame, "STATE : CENTER", (350, 50), font, 1, (0, 0, 255), 3)
-            # else:
-            #     # LEFT_COUNTER+=1
-            #     cv2.putText(frame, "STATE : LEFT", (350, 50), font, 1, (0, 0, 255), 3)
+            gaze_right = Gaze(right_eye.get_eye_region(),mask,gray)
+            gaze_left = Gaze(left_eye.get_eye_region(),mask,gray)
+            gaze_ratio = math.ceil((gaze_right.get_gaze_ratio()+gaze_left.get_gaze_ratio())/2)
+            if gaze_ratio == 2:
+                cv2.putText(frame, "STATE : RIGHT", (350, 50), font, 1, (0, 0, 255), 3)
+            elif gaze_ratio == 1:
+                cv2.putText(frame, "STATE : CENTER", (350, 50), font, 1, (0, 0, 255), 3)
+            else:
+                # LEFT_COUNTER+=1
+                cv2.putText(frame, "STATE : LEFT", (350, 50), font, 1, (0, 0, 255), 3)
         cv2.imshow('frame',frame)
         # cv2.imshow('mask',mask)
         if cv2.waitKey(1) & 0xFF == ord('q'):
