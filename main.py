0e8f612c9f9cf67067572cf9f20f8693c668c69e
Ahmed Hamada <ahmedhamada507@gmail.com>
2023-02-14 18:12:14 +0200

    Add more comments to main and Movement class explaining the functions and improve the code quality

--- main.py
+++ main.py
@@ -11,82 +11,123 @@ from calibration import Calibration
 # Todo Separate Movement class from main.py
 class Movement:
     def __init__(self):
-        self.total_blinks = 0
-        self.left_counter = 0
-        self.right_counter = 0
-        self.center_counter = 0
-        self.eye_direction_frame = 10
-        self.is_right = False
-        self.is_left = False
-        self.is_center = False
-        self.gaze_ratio = 4
-        self.is_running = False
+        """
+        Initialize Movement object
+        """
+        self.__total_blinks = 0
+        self.__left_counter = 0
+        self.__right_counter = 0
+        self.__center_counter = 0
+        self.__eye_direction_frame = 10
+        self.__is_right = False
+        self.__is_left = False
+        self.__is_center = False
+        self.__gaze_ratio = 4
+        self.__is_running = False
 
     def run(self):
-        if 2 <= self.total_blinks <= 3:
-            self.is_running = True
-        elif self.total_blinks > 3:
-            self.total_blinks = 0
-            self.is_running = False
+        """
+        Run the driver function if the total blinks are between 2 and 3 and stop if the total blinks are more than 3.
+        :return: None
+        """
+        if 2 <= self.__total_blinks <= 3:
+            self.__is_running = True
+        elif self.__total_blinks > 3:
+            self.__total_blinks = 0
+            self.__is_running = False
         else:
-            self.is_running = False
+            self.__is_running = False
 
     def driver(self):
+        """
+        Driver function to control the movement.
+        :return: None
+        """
         self.run()
-        if self.is_running:
-            if self.gaze_ratio == 1:
-                self.move_center()
-            elif self.gaze_ratio == 0:
+        if self.__is_running:
+            if self.__gaze_ratio == 1:
+                self.move_forward()
+            elif self.__gaze_ratio == 0:
                 self.move_left()
-            elif self.gaze_ratio == 2:
+            elif self.__gaze_ratio == 2:
                 self.move_right()
             else:
                 self.stop()
 
     def move_counter_reset(self):
-        self.right_counter = 0
-        self.left_counter = 0
-        self.center_counter = 0
+        """
+        Reset direction counters to 0 for a certain number of frames and not blinking.
+        :return: None
+        """
+        self.__right_counter = 0
+        self.__left_counter = 0
+        self.__center_counter = 0
 
-    def move_center(self):
+    def move_forward(self):
+        """
+        Move forward if gaze ratio is 1 for a certain number of frames and not blinking.
+        :return: None
+        """
         # if self.gaze_ratio == 1:
-        self.is_center = True
-        self.center_counter += 1
+        self.__is_center = True
+        self.__center_counter += 1
         cv2.putText(frame, "STATE : FORWARD", (50, 100), FONT, 1, (0, 0, 255), 3)
-        if self.center_counter > self.eye_direction_frame:
+        if self.__center_counter > self.__eye_direction_frame:
             self.stop()
             print('Forward')
 
     def move_left(self):
+        """
+        Move left if gaze ratio is 0 for a certain number of frames and not blinking.
+        :return: None
+        """
         # if self.gaze_ratio == 0:
-        self.is_center = True
-        self.left_counter += 1
+        self.__is_center = True
+        self.__left_counter += 1
         cv2.putText(frame, "STATE : LEFT", (50, 100), FONT, 1, (0, 0, 255), 3)
-        if self.left_counter > self.eye_direction_frame:
+        if self.__left_counter > self.__eye_direction_frame:
             self.stop()
             print('Left')
 
     def move_right(self):
+        """
+        Move right if gaze ratio is 2 for a certain number of frames and not blinking.
+        :return: None
+        """
         # if self.gaze_ratio == 2:
-        self.is_center = True
-        self.right_counter += 1
+        self.__is_center = True
+        self.__right_counter += 1
         cv2.putText(frame, "STATE : RIGHT", (50, 100), FONT, 1, (0, 0, 255), 3)
-        if self.right_counter > self.eye_direction_frame:
+        if self.__right_counter > self.__eye_direction_frame:
             self.stop()
             print('Right')
 
     def stop(self):
+        """
+        Stop if gaze ratio is not 1, 0 or 2 and reset direction counters.
+        :return:
+        """
         cv2.putText(frame, "STATE : STOP", (50, 100), FONT, 1, (0, 0, 255), 3)
-        self.is_center = False
-        self.is_left = False
-        self.is_right = False
+        self.__is_center = False
+        self.__is_left = False
+        self.__is_right = False
         self.move_counter_reset()
 
     def set_gaze_ratio(self, gaze_ratio):
-        self.gaze_ratio = gaze_ratio
+        """
+        Set gaze ratio to a certain value
+        :param gaze_ratio: gaze ratio value
+        :return: None
+        """
+        self.__gaze_ratio = gaze_ratio
 
     def set_total_blinks(self, total_blinks):
-        self.total_blinks = total_blinks % 4
+        """
+        Set total blinks to a certain value
+        :param total_blinks: total blinks value
+        :return: None
+        """
+        self.__total_blinks = total_blinks % 4
 
 
 # Todo Make constants.py file and move all constants there and import them here and in other files as well
@@ -95,79 +136,78 @@ class Movement:
 # Todo Make Unit tests for all functions and classes
 # Todo Improve code readability and quality
 if __name__ == "__main__":
-    TOTAL_BLINKS = 0
-    BLINKS_COUNTER = 0
-    LEFT_COUNTER = 0
-    RIGHT_COUNTER = 0
-    CENTER_COUNTER = 0
-    FLAG = 0
+    # Constants
     CLOSED_EYES_FRAME = 10
     EYE_DIRECTION_FRAME = 10
     MODEL = "shape_predictor_68_face_landmarks.dat"
     FONT = cv2.FONT_HERSHEY_SIMPLEX
-
+    # Variables
     left_eye_thresh = 100
     right_eye_thresh = 100
-    # blinking_threshold = 5.2
-    blinks = []
-    k = True
+    # Objects
     calibrate = Calibration()
     movement = Movement()
     blink = Blink()
+    # Main
     try:
-        cap = cv2.VideoCapture(0)
-        detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor(MODEL)
+        cap = cv2.VideoCapture(0)  # initialize camera
+        detector = dlib.get_frontal_face_detector()  # initialize face detector
+        predictor = dlib.shape_predictor(MODEL)  # initialize landmark detector
 
         while True:
             try:
-                ret, frame = cap.read()
-                frame = cv2.flip(frame, 1)
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                faces = detector(gray)
-                height, width, _ = frame.shape
-                mask = np.zeros((height, width), dtype=np.uint8)
+                ret, frame = cap.read()  # read frame from camera
+                frame = cv2.flip(frame, 1)  # flip frame
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert frame to grayscale
+                faces = detector(gray)  # detect faces in frame
+                height, width, _ = frame.shape  # get frame dimensions
+                mask = np.zeros((height, width), dtype=np.uint8)  # create black mask
 
                 for face in faces:
+                    # Detect face and draw rectangle
                     x, y = face.left(), face.top()
                     x1, y1 = face.right(), face.bottom()
                     cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
-                    landmarks = predictor(gray, face)
-                    left_eye = Eye(frame, 'left', landmarks)
-                    right_eye = Eye(frame, 'right', landmarks)
+                    landmarks = predictor(gray, face)  # detect landmarks on face
+                    left_eye = Eye(frame, 'left', landmarks)  # detect left eye
+                    right_eye = Eye(frame, 'right', landmarks)  # detect right eye
                     # Detect blinking
-                    left_eye_ratio = left_eye.blink_ratio()
-                    right_eye_ratio = right_eye.blink_ratio()
-                    blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
-                    blink.set_blink_ratio(blinking_ratio)
-                    blink.set_blinking_threshold(calibrate.get_cal_blink_threshold())
-
-                    if calibrate.is_calibrated:
-                        TOTAL_BLINKS = blink.count_blinks()
-                        if blink.is_blinking():
-                            cv2.putText(frame, "BLINKING", (50, 150), FONT, 3, (255, 0, 0))
-
-                        cv2.putText(frame, f'Total Blinks: {TOTAL_BLINKS}', (50, 350), FONT, 2, (0, 0, 255), 3)
-
-                    gaze_right = Gaze(right_eye.get_eye_region(), mask, gray)
+                    left_eye_ratio = left_eye.blink_ratio()  # get left eye blink ratio
+                    right_eye_ratio = right_eye.blink_ratio()  # get right eye blink ratio
+                    blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2  # get average blink ratio
+                    blink.set_blink_ratio(blinking_ratio)  # set blink ratio to Blink class
+                    blink.set_blinking_threshold(
+                        calibrate.get_cal_blink_threshold())  # set blink threshold to Blink class
+                    # Detect eye Gaze
+                    gaze_right = Gaze(right_eye.get_eye_region(), mask, gray)  # detect right eye gaze
                     gaze_right.set_threshold(calibrate.get_cal_right_eye_thresh())  # change right eye threshold
-                    gaze_left = Gaze(left_eye.get_eye_region(), mask, gray)
+                    gaze_left = Gaze(left_eye.get_eye_region(), mask, gray)  # detect left eye gaze
                     gaze_left.set_threshold(calibrate.get_cal_left_eye_thresh())  # change left eye threshold
-
                     # Calibrate
                     calibrate.calibrate(gaze_left, gaze_right, blinking_ratio)
                     if calibrate.is_cal_threshold:
-                        cv2.putText(frame, "Calibrating Threshold", (150, 50), FONT, 1, (200, 0, 200), 2)
+                        cv2.putText(frame, "Calibrating Threshold", (150, 50), FONT, 1, (200, 0, 200), 2)  # show Calibrating Threshold message
                     elif calibrate.is_cal_blink:
-                        cv2.putText(frame, "Calibrating Blinking", (150, 50), FONT, 1, (200, 0, 200), 2)
+                        cv2.putText(frame, "Calibrating Blinking", (150, 50), FONT, 1, (200, 0, 200),
+                                    2)  # show Calibrating Blinking message
+                    # Check if calibration is done and start the driver
+                    if calibrate.is_calibrated:
+                        total_blinks = blink.count_blinks()  # Count total blinks
+                        # Check if eyes are blinking
+                        if blink.is_blinking():
+                            cv2.putText(frame, "BLINKING", (50, 150), FONT, 3, (255, 0, 0))  # show blinking message
 
-                    gaze_ratio = math.ceil((gaze_right.get_gaze_ratio() + gaze_left.get_gaze_ratio()) / 2)
-                    movement.set_total_blinks(TOTAL_BLINKS)
-                    movement.set_gaze_ratio(gaze_ratio)
-                    movement.driver()
+                        cv2.putText(frame, f'Total Blinks: {total_blinks}', (50, 350), FONT, 2, (0, 0, 255), 3)  # show total blinks
+                        # Get total gaze ratio by averaging the left and right eye gaze ratio
+                        gaze_ratio = math.ceil((gaze_right.get_gaze_ratio() + gaze_left.get_gaze_ratio()) / 2)
+                        # Run the driver
+                        movement.set_total_blinks(total_blinks)
+                        movement.set_gaze_ratio(gaze_ratio)
+                        movement.driver()
                 cv2.imshow('frame', frame)
                 # cv2.imshow('mask',mask)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):  # Wait for 'q' key to stop the program
                     break
             except Exception as e:
                 print("Error processing the frame:", e)
